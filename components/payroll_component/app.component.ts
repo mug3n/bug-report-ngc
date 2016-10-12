ac33e49a11f7cd985d636a9888c047a25cc7582e
mugen <npavlovic101@gmail.com>
2016-10-12 17:14:17 +0200

    typos

--- components/payroll_component/app.component.ts
+++ components/payroll_component/app.component.ts
@@ -1,63 +1,12 @@
-// import { RateCardsPageComponent } from './rate_cards_page_component/rate.cards.page.component';
-// import { PayrollPageComponent } from './payroll_page_component/payroll.page.component';
-// import { PayrollNavComponent } from './payroll_nav_component/payroll.nav.component';
-// import { TranslateService } from 'ng2-translate';
-// import { LoggerService, LoggingLevels } from '../shared/logger.service';
-// import { Component, OnInit, ViewChild } from '@angular/core';
-// import { INavPage } from './payroll_nav_component/payroll.nav.component';
 import { Component } from '@angular/core';
 
 @Component({
-    // directives: [PayrollNavComponent],
     selector: 'hm-payroll-component',
     template: `
-        <!--
-        <div>
-            <payroll-nav-component
-                [pages]="pages"
-                [rootPage]="rootPage"
-                (showPage)="__showPage($event)"
-            ></payroll-nav-component>
-        </div>
-        <div>
-            <ion-nav [root]="rootPage.component" #content></ion-nav>
-        </div>
-        -->
-        <h1> Payroll Component 22</h1>
+        <h1>App Component 1</h1>
         <hm-input-numeric></hm-input-numeric>
     `,
 })
-export class PayrollComponent {
-    // @ViewChild(Nav) private nav: Nav;
+export class AppComponent {
 
-    // private pages: INavPage[];
-    // private rootPage: INavPage;
-
-    // constructor(
-    //     private translateService: TranslateService,
-    //     private logger: LoggerService,
-    //     private platform: Platform) {
-    // }
-
-    // public ngOnInit() {
-    //     this.rootPage = {
-    //         component: PayrollPageComponent,
-    //         icon: 'chatbubbles',
-    //         title: 'Payroll',
-    //     };
-    //     this.logger.logLevel = LoggingLevels.Debug;
-
-    //     this.pages = [
-    //         this.rootPage,
-    //         {
-    //             component: RateCardsPageComponent,
-    //             icon: 'chatbubbles',
-    //             title: 'Rate cards',
-    //         },
-    //     ];
-    // }
-
-    // public __showPage(page: INavPage): void {
-    //     this.nav.setRoot(page.component);
-    // }
 }
--- components/payroll_component/main.ts
+++ components/payroll_component/main.ts
@@ -1,4 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { PayrollModule } from './app.module';
+import { MainModule } from './app.module';
 
-platformBrowserDynamic().bootstrapModule(PayrollModule);
+platformBrowserDynamic().bootstrapModule(MainModule);
