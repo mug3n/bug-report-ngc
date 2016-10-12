1ab7ea15287a2d59282bd2b044e4a30b11839fc5
mugen <npavlovic101@gmail.com>
2016-10-12 17:16:53 +0200

    naming

--- components/app_component/app.component.ts
+++ components/app_component/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'hm-payroll-component',
+    selector: 'hm-app-component',
     template: `
         <h1>App Component 1</h1>
         <hm-input-numeric></hm-input-numeric>
